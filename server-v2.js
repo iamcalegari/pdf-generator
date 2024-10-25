8bab56a9d4691eff8a7f44336a34b0e714dafe8a
iamcalegari <iamcalegari20@gmail.com>
2024-10-25 13:27:04 -0300

    feat-wip

---
+++ server-v2.js
@@ -0,0 +1,130 @@
+const express = require("express");
+const puppeteer = require("puppeteer");
+const path = require("path");
+const fs = require("fs-extra");
+const handlebars = require("handlebars");
+const data = require("./fixtures/document-example.json");
+
+const app = express();
+
+const readTemplate = (filePath) => {
+  return fs.readFileSync(filePath, "utf8");
+};
+
+// Rota para gerar o PDF
+app.get("/", async (req, res) => {
+  // (async () => {
+  const style = readTemplate(
+    path.join(__dirname, "public-v2", "css", "styles.css")
+  );
+
+  const mainTemplate = readTemplate(
+    path.join(__dirname, "views-v3", "layouts", "main.hbs")
+  );
+
+  const bodyTemplate = readTemplate(
+    path.join(__dirname, "views-v3", "pdf-template.hbs")
+  );
+
+  // Carrega o template
+  const template = handlebars.compile(mainTemplate);
+  const body = handlebars.compile(bodyTemplate);
+
+  // Primeira Passagem: Renderiza o HTML sem números de página
+  const initialHtml = body(data);
+
+  // Inicia o Puppeteer
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Define o conteúdo da página
+  await page.setContent(initialHtml, { waitUntil: "networkidle0" });
+
+  // Aguarda a renderização completa
+  await page.evaluateHandle("document.fonts.ready");
+
+  // Calcula os números de página para cada tópico e subtópico
+  const pageNumbers = await page.evaluate(() => {
+    const elements = document.querySelectorAll("[id]");
+    const pageNumbers = {};
+
+    elements.forEach((el) => {
+      const id = el.id;
+      // Calcula o número da página baseado na posição do elemento
+      const rect = el.getBoundingClientRect();
+      const pageNumber =
+        Math.floor((window.scrollY + rect.top) / window.innerHeight) + 1;
+      pageNumbers[id] = pageNumber;
+    });
+
+    return pageNumbers;
+  });
+
+  // Atualiza os dados com os números de página calculados
+  function updateTopics(topics) {
+    topics.forEach((topic) => {
+      topic.pageNumber = pageNumbers[topic.id];
+      if (topic.subtopics) {
+        topic.subtopics.forEach((subtopic) => {
+          subtopic.pageNumber = pageNumbers[subtopic.id];
+        });
+      }
+    });
+  }
+
+  updateTopics(data.topics);
+
+  // Segunda Passagem: Renderiza o HTML com os números de página atualizados
+  const finalHtml = body(data);
+
+  const html = template({ styles: style, body: finalHtml });
+
+  // Define o conteúdo da página novamente
+  await page.setContent(html, { waitUntil: "load" });
+
+  // Aguarda a renderização completa
+  await page.evaluateHandle("document.fonts.ready");
+
+  // Atualiza o sumário com os números de página corretos
+  await page.evaluate(() => {
+    const tocEntries = document.querySelectorAll(".toc-page-number");
+    tocEntries.forEach((span) => {
+      const id = span.getAttribute("data-id");
+      const el = document.getElementById(id);
+      if (el) {
+        const rect = el.getBoundingClientRect();
+        const pageNumber =
+          Math.floor((window.scrollY + rect.top) / window.innerHeight) + 1;
+        span.textContent = pageNumber;
+      }
+    });
+  });
+
+  // Gera o PDF final
+  const pdfBuffer = await page.pdf({
+    // path: "documento_final.pdf",
+    format: "A4",
+    printBackground: true,
+    displayHeaderFooter: true,
+    margin: {
+      top: "0",
+      bottom: "0",
+      left: "0",
+      right: "0",
+    },
+  });
+
+  await browser.close();
+
+  console.log("PDF gerado com sucesso!");
+
+  res.type("application/pdf");
+  res.send(pdfBuffer);
+  // })();
+});
+
+const PORT = process.env.PORT || 3008;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta http://localhost:${PORT}`);
+});
--- server.js
+++ server.js
@@ -6,51 +6,60 @@ const handlebars = require("handlebars");
 const document = require("./fixtures/document.json");
 
 const app = express();
+app.use(express.static(path.join(__dirname, "public")));
 
 const readTemplate = (filePath) => {
   return fs.readFileSync(filePath, "utf8");
 };
 
-const getTopics = (document) => {
-  const inspection = {
-    name: "VISTORIA DA EDIFICAÇÃO",
-    description: document.propertyDetails.description,
-  };
+// const getTopics = (document) => {
+//   const inspection = {
+//     name: "VISTORIA DA EDIFICAÇÃO",
+//     description: document.propertyDetails.description,
+//   };
 
-  const rooms = document.roomsDetails.reduce((acc, curr) => {
-    acc.push({
-      name: curr.name,
-      description: curr.description,
-      photos: curr.photos,
-    });
+//   const rooms = document.roomsDetails.reduce((acc, curr) => {
+//     acc.push({
+//       name: curr.name,
+//       description: curr.description,
+//       photos: curr.photos,
+//     });
 
-    return acc;
-  }, []);
+//     return acc;
+//   }, []);
 
-  inspection["rooms"] = rooms;
+//   inspection["rooms"] = rooms;
 
-  const topics = document.topicsDetails.reduce((acc, curr) => {
-    const template = handlebars.compile(curr.template);
+//   const topics = document.topicsDetails.reduce((acc, curr) => {
+//     const template = handlebars.compile(curr.template);
 
-    const topicHtml = template({
-      ...curr.data,
-    });
+//     const topicHtml = template({
+//       ...curr.data,
+//     });
 
-    acc.push({
-      name: curr.name,
-      text: topicHtml,
-    });
+//     acc.push({
+//       name: curr.name,
+//       text: topicHtml,
+//     });
 
-    return acc;
-  }, []);
+//     return acc;
+//   }, []);
 
-  topics.splice(2, 0, inspection);
+//   topics.splice(2, 0, inspection);
 
-  return { topics };
-};
+//   return { topics };
+// };
 
 // Rota para gerar o PDF
+
 app.get("/", async (req, res) => {
+  const timbre =
+    "data:image/png;base64, " +
+    fs.readFileSync(
+      path.join(__dirname, "public", "css", "timbre.png"),
+      "base64"
+    );
+
   const style = readTemplate(
     path.join(__dirname, "public", "css", "styles.css")
   );
@@ -59,8 +68,12 @@ app.get("/", async (req, res) => {
     path.join(__dirname, "views-v2", "layouts", "main.hbs")
   );
 
+  // const bodyTemplate = readTemplate(
+  //   path.join(__dirname, "views-v2", "pdf-template.hbs")
+  // );
+
   const bodyTemplate = readTemplate(
-    path.join(__dirname, "views-v2", "pdf-template.hbs")
+    path.join(__dirname, "views-v2", "topic-template.hbs")
   );
 
   const template = handlebars.compile(mainTemplate);
@@ -70,32 +83,32 @@ app.get("/", async (req, res) => {
     return a === b;
   });
 
-  const bodyHtml = body(getTopics(document));
+  const bodyHtml = body();
 
   const html = template({
     body: bodyHtml,
     title: "PDF",
     styles: style,
+    timbre: timbre,
   });
 
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
 
-  await page.setContent(html, { waitUntil: "domcontentloaded" });
+  await page.setContent(html, { waitUntil: "networkidle0" });
 
   // Adicionar numeração de páginas
   const pdfBuffer = await page.pdf({
     format: "A4",
     printBackground: true,
     displayHeaderFooter: true,
-    headerTemplate: `<div></div>`,
-    footerTemplate: `<div style="font-size:10px; text-align: center; width: 100%;">
-                            <span class="pageNumber"></span> / <span class="totalPages"></span>
+    // headerTemplate: `<div><h1>HEADER</h1></div>`,
+    footerTemplate: `<div style="font-size:12pt; text-align: end; width: 88%; position: absolute; bottom: 60px;">
+                            <span class="pageNumber"></span></span>
                          </div>`,
-    margin: {
-      top: "20mm",
-      bottom: "20mm",
-    },
+    tagged: true,
+    waitForFonts: true,
+    preferCSSPageSize: true,
   });
 
   await browser.close();
